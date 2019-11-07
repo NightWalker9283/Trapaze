0cfb32c937ae40468f2628cf6638aa6e4354906b
user <>
2019-11-07 18:05:00 +0900

    パラシュート完成(本物)

---
+++ Assets/Scripts/Parachute.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Parachute : MonoBehaviour
+{
+    public float coefficient = 10f;   // 空気抵抗係数
+    [SerializeField] Rigidbody Player;
+    Cloth polyCloth;
+
+    private void Start()
+    {
+        polyCloth = GetComponentInChildren<Cloth>();
+    }
+
+    void FixedUpdate()
+    {
+        // 空気抵抗を与える
+        var resistance = GetComponent<Rigidbody>().velocity;
+        resistance.Set(resistance.x * -coefficient*0.1f, resistance.y * -coefficient, resistance.z * -coefficient * 0.1f);
+        GetComponent<Rigidbody>().AddForce(resistance);
+    }
+
+    private void Update()
+    {
+        if (!polyCloth.enabled && Player.velocity.y > -1f)
+        {
+            polyCloth.enabled = true;
+            StartCoroutine(desableParachute());
+        }
+    }
+    IEnumerator desableParachute()
+    {
+        yield return new WaitForSeconds(2f);
+        gameObject.SetActive(false);
+    }
+
+}
