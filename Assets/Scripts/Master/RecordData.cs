586ac66d90a42fce1e09999bfd21adb998824638
user <>
2019-11-12 09:04:48 +0900

    セーブ、ロード機能実装 　設定画面のデータのセーブ・ロード実装完了 　成績画面外装完了。成績画面に表示されるモードは現在実装済みのモードを拾ってきて自動生成されるよう実装完了。残り各モードの成績表示 PlayingManager、GameMasterに自己のインスタンスをstaticで持たせ、それに応じて他のクラスからの参照関係も変更 音量調節、ON/OFF切り替え実装 GameMaster,PlayingManagerに他のクラスからいくつかメンバを移し、依存関係を少し整理

---
+++ Assets/Scripts/Master/RecordData.cs
@@ -0,0 +1,30 @@
+﻿[System.Serializable]
+public class RecordData
+{
+    public int game_mode_id;
+    public float max_distance;
+    public float timespan_maxdistance;
+    public float min_distance;
+    public float timespan_mindistance;
+    public int play_count;
+    public float total_time;
+
+    public RecordData(int game_mode_id,
+        float max_distance,
+        float timespan_maxdistance,
+        float min_distance,
+        float timespan_mindistance,
+        int play_count,
+        float total_time)
+    {
+        this.game_mode_id = game_mode_id;
+        this.max_distance = max_distance;
+        this.timespan_maxdistance = timespan_maxdistance;
+        this.min_distance = min_distance;
+        this.timespan_mindistance = timespan_mindistance;
+        this.play_count = play_count;
+        this.total_time = total_time;
+    }
+
+
+}
---
+++ Assets/Scripts/UI/Title/ToggleWindow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleWindow : MonoBehaviour
+{
+    [SerializeField] GameObject targetWindow;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void ToggleWindowProc()
+    {
+        targetWindow.SetActive(!targetWindow.activeSelf);
+    }
+}
