369fcce6a89c05947b87e5180b855d1dca8d5344
user <>
2019-11-16 05:09:06 +0900

    うんこちゃんモデル完成。一応Playerのコンポーネントすべてコピー済み。これからPlayerと置き換えて動くかチェック

---
+++ Assets/Scripts/etc/CopyComponent.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class CopyComponent : MonoBehaviour
+{
+    [SerializeField] bool Run=false;
+    [SerializeField] Transform Origin, Target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Run)
+        {
+            
+
+
+
+
+            Run = false;
+        }
+    }
+}
