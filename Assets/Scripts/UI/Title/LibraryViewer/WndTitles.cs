69e42b2c4242be0397817bc6e8fcf1890b9b61cd
user <>
2019-12-14 02:56:04 +0900

    ・ライブラリ閲覧機能関連のフォルダ名が「Library」であるとSourcetreeが認識できないようで、前回のコミットで漏れていた為「LibraryViewer」に変更

---
+++ Assets/Scripts/UI/Title/LibraryViewer/WndTitles.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+public class WndTitles : MonoBehaviour
+{
+    [SerializeField] ToggleGroup tggTitles;
+    [SerializeField] Text txtNameTitle, txtConditionTitle, txtDescriptionTitle;
+
+    Titles titles;
+    // Start is called before the first frame update
+    void Start()
+    {
+        titles = GameMaster.gameMaster.titles;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void UpdateTitleInfo(bool isOn)
+    {
+        if (isOn)
+        {
+           
+            var id = tggTitles.ActiveToggles().FirstOrDefault().GetComponent<ListElementTitle>().id;
+            var titleObject = titles.allTitles.Find(dt => dt.id == id);
+            if (titleObject != null)
+            {
+                txtNameTitle.text = titleObject.name;
+                txtConditionTitle.text = titleObject.condition;
+                txtDescriptionTitle.text = titleObject.description;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Title/LibraryViewer/WndVoices.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+public class WndVoices : MonoBehaviour
+{
+    [SerializeField] ToggleGroup tggVoices;
+    AudioSource audioSource;
+    List<string> acquiredVoices;
+    // Start is called before the first frame update
+    void Start()
+    {
+        acquiredVoices = GameMaster.gameMaster.acquiredVoices;
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void PlayVoice()
+    {
+        audioSource.Stop();
+        //Resources.UnloadUnusedAssets();
+        var path = tggVoices.ActiveToggles().FirstOrDefault().GetComponent<ListElementVoice>().path;
+
+        var ac=Resources.Load<AudioClip>(path);
+        audioSource.PlayOneShot(ac);
+        
+    }
+}
