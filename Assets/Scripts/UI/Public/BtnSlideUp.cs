ee3a0175f880d5cad71417a569267dd08c69e7ac
user <>
2019-12-18 18:09:21 +0900

    獲得称号ウィンドウのタイトルバーをタッチするとウィンドウがON/OFFするよう改良。

---
+++ Assets/Scripts/UI/Public/BtnSlideUp.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.TerrainAPI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BtnSlideUp : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    
+    Canvas cvsPublic;
+    RectTransform rectCvsPublic;
+    Vector2 sizeCvsPuvlic;
+    Vector3 startPosCvs, endPosCvs;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        
+        cvsPublic = PlayingManager.playingManager.cvsPublic;
+        rectCvsPublic = cvsPublic.GetComponent<RectTransform>();
+        sizeCvsPuvlic = rectCvsPublic.sizeDelta;
+        startPosCvs = rectCvsPublic.position;
+        endPosCvs = new Vector3(startPosCvs.x, startPosCvs.y + sizeCvsPuvlic.y - 30);
+    }
+  
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnBeginDrag(PointerEventData e)
+    {
+        StartSlide();
+    }
+    // ドラック中に呼ばれる.
+    public void OnDrag(PointerEventData eventData)
+    {
+        
+    }
+
+    // ドラックが終了したとき呼ばれる.
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        
+    }
+    void StartSlide()
+    {
+        StartCoroutine(MonitorSlide());
+    }
+
+    IEnumerator MonitorSlide()
+    {
+        var TapPos = DragMonitor.TapPosition;
+        while (DragMonitor.Drag)
+        {
+            var dragLen= (DragMonitor.DragPosition.y - TapPos.y)*sizeCvsPuvlic.y;
+            var newPosCvsY = startPosCvs.y + dragLen;
+            newPosCvsY = Mathf.Clamp(newPosCvsY, startPosCvs.y, endPosCvs.y);
+            rectCvsPublic.position =
+                new Vector3(startPosCvs.x, newPosCvsY);
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Public/TitleBar.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TitleBar : MonoBehaviour
+{
+    [SerializeField] Mask mask;
+    [SerializeField] CanvasGroup cvsgtitleElements;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(OnClick);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnClick()
+    {
+        if (mask.showMaskGraphic)
+        {
+            mask.showMaskGraphic = false;
+            cvsgtitleElements.alpha=0f;
+        }
+        else
+        {
+            mask.showMaskGraphic = true;
+            cvsgtitleElements.alpha=1f;
+        }
+
+    }
+
+}
