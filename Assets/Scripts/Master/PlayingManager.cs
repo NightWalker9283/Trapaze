6aa5de2575f50e4eff791396c794633f18959667
user <>
2020-01-13 05:51:09 +0900

    使用していないアセット・テクスチャーを削除 テライン用テクスチャーの種類減らし、テラインに塗り直し Ver.1.35で初期エフェクト中キャンバスのactiveSelfをfalseにしていたことで、キャンバス上のオブジェクトの上で走るコルーチンが停止しており、速度表示が0から変化しないバグがあったため修正（Graphic RaycasterをON・OFFすることでキャンバスに触れなくした）

--- Assets/Scripts/Master/PlayingManager.cs
+++ Assets/Scripts/Master/PlayingManager.cs
@@ -345,8 +345,10 @@ public class PlayingManager : MonoBehaviour
     {
         if (!isTutorial)
         {
-            cvsPlayer.gameObject.SetActive(false);
-            cvsPublic.gameObject.SetActive(false);
+            var grCvsPlayer = cvsPlayer.GetComponent<GraphicRaycaster>();
+            var grCvsPublic = cvsPublic.GetComponent<GraphicRaycaster>();
+            grCvsPlayer.enabled = false;
+            grCvsPublic.enabled = false;
             cmrUiPlayer.gameObject.SetActive(false);
             CanvasTop.canvasTop.FadeinScene();
             vcamFace.gameObject.SetActive(true);
@@ -361,8 +363,8 @@ public class PlayingManager : MonoBehaviour
             vcamFace.gameObject.SetActive(false);
             mayoCount = gameMaster.gameMode.initialMayoCnt;
             rb_Trapeze.isKinematic = false;
-            cvsPlayer.gameObject.SetActive(true);
-            cvsPublic.gameObject.SetActive(true);
+            grCvsPlayer.enabled = true;
+            grCvsPublic.enabled = true;
         }
         CanvasTop.canvasTop.FadeinScene();
         cmrUiPlayer.gameObject.SetActive(true);
