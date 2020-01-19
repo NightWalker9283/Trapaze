1e2ae0b9a53d3e17d40d032a21145a4d2c64dc13
user <>
2020-01-19 14:23:27 +0900

    実機でツイートボタンが動作しないバグを修正

--- Assets/Scripts/etc/TweetManager.cs
+++ Assets/Scripts/etc/TweetManager.cs
@@ -86,9 +86,9 @@ namespace TweetWithScreenShot
             // ツイッター投稿用URL
             string TweetURL = "http://twitter.com/intent/tweet?text=" + text + hashtags;
 
-#if UNITY_WEBGL && !UNITY_EDITOR
-            Application.ExternalEval(string.Format("window.open('{0}','_blank')", TweetURL));
-#elif UNITY_EDITOR
+//#if UNITY_WEBGL && !UNITY_EDITOR
+            //Application.ExternalEval(string.Format("window.open('{0}','_blank')", TweetURL));
+#if UNITY_EDITOR
             System.Diagnostics.Process.Start (TweetURL);
 #else
             Application.OpenURL(TweetURL);
